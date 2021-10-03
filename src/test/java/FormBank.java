b481a4201fa5f332233f4d37f77749b7ed164ad6
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-10-03 23:25:54 +0700

    Update FormBank.java

--- src/test/java/FormBank.java
+++ src/test/java/FormBank.java
@@ -2,13 +2,21 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FormBank {
 
     WebDriver driver;
+//    ChromeOptions options = new ChromeOptions();
+//    options.addArguments("--disable-dev-shm-usage");
+//    options.addArguments("--no-sandbox");
+//    options.addArguments("--headless");
+//    driver = new ChromeDriver(options);
 
     @BeforeAll
     static void setupClass() {
@@ -28,7 +36,14 @@ public class FormBank {
     }
 
     @Test
-    void test() {
-        // Your test logic here
+    void shouldTestV1() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[name=name]")).sendKeys("Павел");
+        driver.findElement(By.cssSelector("[name=phone]")).sendKeys("+79238456245");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String actualMessage = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        String expectedMessage = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expectedMessage, actualMessage.strip());
     }
 }
