358c2d5be7d7ae9a6d477ffed5262fe4d01a710d
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-10-04 01:01:24 +0700

    Update FormBank.java

--- src/test/java/FormBank.java
+++ src/test/java/FormBank.java
@@ -1,50 +1,62 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.BeforeEach;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.*;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import io.github.bonigarcia.wdm.WebDriverManager;
+//import org.openqa.selenium.chrome.ChromeOptions;
+//
+//import static org.junit.jupiter.api.Assertions.*;
 
 public class FormBank {
 
-    WebDriver driver;
-
-    @BeforeAll
-    static void setupClass() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeEach
-    void setupTest() {
-        driver = new ChromeDriver();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-    }
-
-    @AfterEach
-    void teardown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
-
+    //    WebDriver driver;
+//
+//    @BeforeAll
+//    static void setupClass() {
+//        WebDriverManager.chromedriver().setup();
+//    }
+//
+//    @BeforeEach
+//    void setupTest() {
+//        driver = new ChromeDriver();
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--disable-dev-shm-usage");
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--headless");
+//        driver = new ChromeDriver(options);
+//    }
+//
+//    @AfterEach
+//    void teardown() {
+//        if (driver != null) {
+//            driver.quit();
+//        }
+//    }
+//
+//    @Test
+//    void shouldTestV1() {
+//        driver.get("http://localhost:9999");
+//        driver.findElement(By.cssSelector("[name=name]")).sendKeys("Павел");
+//        driver.findElement(By.cssSelector("[name=phone]")).sendKeys("+79238456245");
+//        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+//        driver.findElement(By.cssSelector("button")).click();
+//        String actualMessage = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+//        String expectedMessage = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+//        assertEquals(expectedMessage, actualMessage.strip());
+//    }
     @Test
-    void shouldTestV1() {
-        driver.get("http://localhost:9999");
-        driver.findElement(By.cssSelector("[name=name]")).sendKeys("Павел");
-        driver.findElement(By.cssSelector("[name=phone]")).sendKeys("+79238456245");
-        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        driver.findElement(By.cssSelector("button")).click();
-        String actualMessage = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
-        String expectedMessage = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        assertEquals(expectedMessage, actualMessage.strip());
+    void shouldTestV2() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Павел К");
+        $("[data-test-id=phone] input").setValue("+79238456245");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
