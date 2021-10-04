f5a2bc8320843671534ccd38fe22db1b62218e45
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-10-04 23:56:57 +0700

    Crash test form

--- src/test/java/FormBank.java
+++ src/test/java/FormBank.java
@@ -7,7 +7,7 @@ public class FormBank {
     @Test
     void shouldTestV1() {
         open("http://localhost:9999");
-        $("[data-test-id=name] input").setValue("Павел К");
+        $("[data-test-id=name] input").setValue("Павел G");
         $("[data-test-id=phone] input").setValue("+79238456245");
         $("[data-test-id=agreement]").click();
         $("button").click();
